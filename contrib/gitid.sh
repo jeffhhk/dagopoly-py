6ff121ea1d5b821d004f9f9466f4640a96f57d4c
Jeff Henrikson <79888432+jeffhhk@users.noreply.github.com>
2023-07-14 15:41:42 -0700

    implement: improved gitid.sh

--- contrib/gitid.sh
+++ contrib/gitid.sh
@@ -1,17 +1,26 @@
 #!/bin/bash
+set -eu
 h=$(git log -1 --pretty=%h)
 
+set +e      # grep -c thinks no hits is an error.  Sigh.
 numAdded=$(git status --porcelain | grep -c ^"A ")
 numModified=$(git status --porcelain | grep -c ^" M")
 numDeleted=$(git status --porcelain | grep -c ^"D ")
 numUntracked=$(git status --porcelain | grep -c ^"??")
 numTotal=$(($numAdded + $numDeleted + $numModified))
+set -e
 
-if [ $numTotal -gt 0 ]
+# _XX: Indicates that project code was modified at the time of run
+# _X:  Indicates that project files were added or deleted at the time of run
+
+if [ $numModified -gt 0 ]
 then
-suffix="_X"
+  suffix="_XX"
+elif [ $numTotal -gt 0 ]
+then
+  suffix="_X"
 else
-suffix=""
+  suffix=""
 fi
 
 echo "${h}${suffix}"
